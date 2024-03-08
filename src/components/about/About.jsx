796a25db36a52cafb922507d44a31b803bb1d256
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-03-08 21:09:37 +0500

    banner has been created 1

---
+++ src/components/about/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import c from './About.module.scss'
+
+const About = () => {
+  return (
+    <div>
+        <h2>BIZ <span style={{color: "rgb(119, 230, 243)"}}>HAQIMIZDA</span></h2>
+        <p>
+        HLB Motors - Ommabop elektromobillarni O'zbekistonga olib kirish va sotishga ixtisoslashgan dinamik avto salon. Ushbu kompaniya O'zbekistonning avtomobil bozorida mashhur BYD brendining innovatsion va ekologik toza avtomobillarini taklif qilib kelmoqda. Toshkent shahrida strategik jihatdan joylashgan ushbu kompaniya mahalliy mijozlarga BYD avtomobillarining ilg'or texnologiyasi va sifatli mahoratini taqdim etadi. To'g'ridan-to'g'ri Xitoydan import qilish orqali ular har bir avtomobilning elektr harakatlanishdagi so'nggi yutuqlari va innovatsiyalarini o'zida mujassamlashtirganini ta'minlaydi, bu BYD ning barqarorlik va ishlashga sodiqligini aks ettiradi.
+        </p>
+        <p>
+        Toshkentning qoq markazida joylashgan ushbu kompaniya BYD ishqibozlari va yuqori darajadagi avtomobil yechimlarini izlayotgan atrof-muhitga e'tiborli haydovchilar uchun markaz bo'lib xizmat qiladi. Eng soʻnggi texnologik xususiyatlarni oʻzida mujassam etgan va xilma-xil modellarni taklif etuvchi yuqori sifatli avtomobillar bilan taʼminlashga eʼtibor qaratgan holda, ular Oʻzbekiston avtomobil bozorining turli afzalliklarini qondirishga qaratilgan. BYD ning global mukammalligini mahalliy foydalanish imkoniyati bilan uyg'unlashtirgan holda, ushbu kompaniya Toshkentdagi avtomobil landshaftini shakllantirishda muhim rol o'ynaydi, O'zbekistonning aqlli iste'molchilariga zamonaviy, ekologik toza haydash tajribasini taqdim etadi.
+        </p>
+    </div>
+  )
+}
+
+export default About
--- src/pages/main-view/Main.jsx
+++ src/pages/main-view/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/navbar/Navbar'
 import Banner from '../../components/banner/Banner'
+import About from '../../components/about/About'
 
 const Main = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <About  />
     </>
   )
 }
